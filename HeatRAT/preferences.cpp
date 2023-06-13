16b792ac1d426430b356635b742c4c6e80bf876a
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-06-13 14:09:13 +0300

    Update preferences.cpp

--- HeatRAT/preferences.cpp
+++ HeatRAT/preferences.cpp
@@ -17,10 +17,23 @@
 
 #include "main.h"
 #include "transfer.h"
+#include "sessionSelecter.h"
+#include <Lmcons.h>
+#include <direct.h>
 
-void Preferences(string logo)
+std::string GetUser()
 {
+	TCHAR s[UNLEN + 1];
+	DWORD sizethis = UNLEN + 1;
+	GetUserName((TCHAR*)s, &sizethis);
+	std::wstring ws(s);
+	std::string userName(ws.begin(), ws.end());
+	return userName;
+}
 
+
+void Sessions(string logo)
+{
 	HANDLE  hConsole;
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	FlushConsoleInputBuffer(hConsole);
@@ -32,8 +45,23 @@ void Preferences(string logo)
 	string something = "";
 	string curentUSER = "";
 	logoShow = true;
+	string ip = "";
+	string pc = "";
+	string use = "";
+	string date = "";
+	string mac = "";
+	string os = "";
+	string lang = "";
+	string curentUsers = R"U( )U";
+	int i = 0;
+	int counte = 0;
 	system("cls");
-	string comands_preferences = "Modules\\Commands\\Preferences.txt";
+	string user = "Modules\\Grabbed\\users.txt";
+	string comands_sessions = "Modules\\Commands\\Sessions.txt";
+	string pcUser = GetUser();
+	string seesionPath = "C:/Users/" + pcUser + "/AppData/Local/Temp/Windows_cache_34266834736";
+	int result = _mkdir(seesionPath.c_str());
+	seesionPath += "/cache.txt";
 
 	while (true)
 	{
@@ -44,10 +72,79 @@ void Preferences(string logo)
 			cout << logo << endl;
 			col = 9;
 			SetConsoleTextAttribute(hConsole, col);
-			printf("%40s-----------------------------------------\n", space.c_str());
-			printf("%40s|              Preferences              |\n", space.c_str());
-			printf("%40s-----------------------------------------\n", space.c_str());
-			printf("Hi %s!!!\n>>> !help ==for==> command list\n", curentUSER.c_str());
+			printf("--------------------------------------------------------------------------------------------------------------------------\n");
+			printf("|                                                        Sessions                                                         |\n");
+			printf("--------------------------------------------------------------------------------------------------------------------------\n");
+			printf("|            User|              PC|              IP|               MAC|            Install_date|           OS|   Language|\n");
+			printf("--------------------------------------------------------------------------------------------------------------------------\n");
+			ifstream file(user);
+			if (file.is_open())
+			{
+				while (getline(file, line))
+				{
+					if (counte == 0 + (7 * i))
+						use = line;
+					if (counte == 1 + (7 * i))
+						pc = line;
+					if (counte == 2 + (7 * i))
+						ip = line;
+					if (counte == 3 + (7 * i))
+						mac = line;
+					if (counte == 4 + (7 * i))
+						date = line;
+					if (counte == 5 + (7 * i))
+						os = line;
+					if (counte == 6 + (7 * i))
+					{
+						lang = line;
+						printf("|%16s|%16s|%16s|%18s|%24s|%13s|%11s|\n", use.c_str(), pc.c_str(), ip.c_str(), mac.c_str(), date.c_str(), os.c_str(), lang.c_str());
+						printf("--------------------------------------------------------------------------------------------------------------------------\n");
+						i++;
+					}
+					counte++;
+				}
+			}
+			else
+			{
+
+				col = 4;
+				SetConsoleTextAttribute(hConsole, col);
+				printf("Fatal ERROR!!!\n");
+			}
+			file.close();
+			line = "";
+			try 
+			{
+				ifstream file(seesionPath);
+				if (file.is_open()) 
+				{
+					while (getline(file, line))
+					{
+						if (counte == 0 + (7 * i))
+							use = line;
+						if (counte == 1 + (7 * i))
+							pc = line;
+						if (counte == 2 + (7 * i))
+							ip = line;
+						if (counte == 3 + (7 * i))
+							mac = line;
+						if (counte == 4 + (7 * i))
+							date = line;
+						if (counte == 5 + (7 * i))
+							os = line;
+						if (counte == 6 + (7 * i))
+						{
+							lang = line;
+							printf("|%16s|%16s|%16s|%18s|%24s|%13s|%11s|\n", use.c_str(), pc.c_str(), ip.c_str(), mac.c_str(), date.c_str(), os.c_str(), lang.c_str());
+							printf("--------------------------------------------------------------------------------------------------------------------------\n");
+							i++;
+						}
+						counte++;
+					}
+				}
+			}
+			catch (...){}
+			printf("Hi %s!!!\n   !help ==for==> command list\n", curentUSER.c_str());
 		}
 		col = 6;
 		SetConsoleTextAttribute(hConsole, col);
@@ -58,7 +155,7 @@ void Preferences(string logo)
 		logoShow = false;
 		if (command == "!help")
 		{
-			Help(comands_preferences);
+			Help(comands_sessions);
 		}
 		if (command == "!menu")
 		{
@@ -66,43 +163,179 @@ void Preferences(string logo)
 			logoShow = true;
 			break;
 		}
-		if (command == "!network")
+		if (command == "!open")
 		{
+			cout << "\n\n";
 			col = 8;
 			SetConsoleTextAttribute(hConsole, col);
-			cout << "\n\n";
-			printf("testing...\n");
+			printf("User: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin.clear();
+			cin.ignore(32767, '\n');
+			getline(cin, use);
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("PC: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin.clear();
+			//cin.ignore(32767, '\n');
+			getline(cin, pc);
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("IP: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin >> ip;
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("MAC: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin >> mac;
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("Install_date: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin.clear();
+			cin.ignore(32767, '\n');
+			getline(cin, date);
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("OS: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin.clear();
+			//cin.ignore(32767, '\n');
+			getline(cin, os);
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("Language: ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin >> lang;
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("opening...\n");
 			printf("[");
 			for (int i = 0; i <= 60; i++)
 			{
 				printf("=");
-				Sleep(100);
+				Sleep(100 * (i / 5));
 			}
 			printf("]\n");
-			srand(time(NULL));
-			int speed = rand() % (100 - 10) + 10;
-			printf("download ===> %d\n", speed);
-			printf("upload ===> %d", speed + rand() % (20 - -3) + -3);
+			ofstream file(user, ios::app);
+			if (file.is_open())
+			{
+				file << use << endl;
+				file << pc << endl;
+				file << ip << endl;
+				file << mac << endl;
+				file << date << endl;
+				file << os << endl;
+				file << lang << endl;
+				col = 2;
+				SetConsoleTextAttribute(hConsole, col);
+				printf("done!!!\n");
+				col = 9;
+				SetConsoleTextAttribute(hConsole, col);
+			}
+			else
+			{
+
+				col = 4;
+				SetConsoleTextAttribute(hConsole, col);
+				printf("Fatal ERROR!!!\n");
+			}
+			file.close();
 			cout << "\n\n";
+			system("pause");
+			system("cls");
+			logoShow = true;
+			Sessions(logo);
+			break;
 		}
-		if (command == "!reset")
+		if (command == "!close")
 		{
-			string logs = "Modules\\Grabbed\\logs.txt";
-			string user = "Modules\\Grabbed\\users.txt";
-			string prefrences = "Modules\\Preferences\\ports.txt";
+			cout << "\n\n";
+			i = 0;
+			counte = 0;
 			col = 8;
 			SetConsoleTextAttribute(hConsole, col);
+			printf("Session number (remember everything starts from 0): ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin >> i;
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			ifstream file(user);
+			if (file.is_open())
+			{
+				while (getline(file, line))
+				{
+					if (counte >= 0 + (8 * i) && counte <= 7 + (8 * i))
+					{
+					}
+					else
+					{
+						curentUsers.insert(curentUsers.length() - 1, line + "\n");
+					}
+					counte++;
+				}
+			}
+			else
+			{
+
+				col = 4;
+				SetConsoleTextAttribute(hConsole, col);
+				printf("Fatal ERROR!!!\n");
+			}
+			curentUsers.pop_back();
+			file.close();
+			line = "";
+			ofstream newfile;
+			newfile.open(user);
+			if (newfile.is_open())
+			{
+				newfile << curentUsers;
+				col = 2;
+				SetConsoleTextAttribute(hConsole, col);
+				printf("session number %d is closed\n", i);
+			}
+			else
+			{
+
+				col = 4;
+				SetConsoleTextAttribute(hConsole, col);
+				printf("Fatal ERROR!!!\n");
+			}
+			newfile.close();
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			system("cls");
+			logoShow = true;
+			Sessions(logo);
+			break;
 			cout << "\n\n";
-			printf("wait...\n");
+		}
+
+		if (command == "!closeAll")
+		{
+			cout << "\n\n";
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("closing...\n");
 			printf("[");
 			for (int i = 0; i <= 60; i++)
 			{
 				printf("=");
-				Sleep(5);
+				Sleep(2);
 			}
 			printf("]\n");
 			ofstream newfile;
-			newfile.open(prefrences);
+			newfile.open(user);
 			if (newfile.is_open())
 			{
 				col = 2;
@@ -117,166 +350,146 @@ void Preferences(string logo)
 				printf("Fatal ERROR!!!\n");
 			}
 			newfile.close();
-			ofstream newUser;
-			newUser.open(user);
-			if (newUser.is_open())
-			{
-				col = 2;
-				SetConsoleTextAttribute(hConsole, col);
-				printf("done!!!\n");
-			}
-			else
-			{
-
-				col = 4;
-				SetConsoleTextAttribute(hConsole, col);
-				printf("Fatal ERROR!!!\n");
-			}
-			newUser.close();
-			ofstream newLogs;
-			newLogs.open(logs);
-			if (newLogs.is_open())
-			{
-				newLogs << "EMPTY";
-				col = 2;
-				SetConsoleTextAttribute(hConsole, col);
-				printf("done!!!\n");
-			}
-			else
-			{
-
-				col = 4;
-				SetConsoleTextAttribute(hConsole, col);
-				printf("Fatal ERROR!!!\n");
-			}
-			newLogs.close();
-			col = 2;
-			SetConsoleTextAttribute(hConsole, col);
-			printf("done!!!\n");
-			col = 9;
+			col = 8;
 			SetConsoleTextAttribute(hConsole, col);
 			system("pause");
 			system("cls");
 			logoShow = true;
+			Sessions(logo);
 			break;
-		}
-		if (command == "!update")
-		{
-			col = 8;
-			SetConsoleTextAttribute(hConsole, col);
-			cout << "\n\n";
-			printf("checking...\n");
-			printf("[");
-			for (int i = 0; i <= 60; i++)
-			{
-				printf("=");
-				Sleep(100);
-			}
-			printf("]\n");
-			col = 2;
-			SetConsoleTextAttribute(hConsole, col);
-			printf("latest version installed\n");
-			col = 9;
-			SetConsoleTextAttribute(hConsole, col);
 			cout << "\n\n";
 		}
-		if (command == "!errors")
+		if (command == "!test")
 		{
-			col = 8;
-			SetConsoleTextAttribute(hConsole, col);
-			cout << "\n\n";
-			printf("checking...\n");
-			printf("[");
-			for (int i = 0; i <= 60; i++)
-			{
-				printf("=");
-				Sleep(2);
-			}
-			printf("]\n");
+			i = 0;
+			counte = 0;
+			int test = 0;
 			srand(time(NULL));
-			int max = rand() % (8 - 0) + 0;
+			cout << "\n\n";
+
 			col = 8;
 			SetConsoleTextAttribute(hConsole, col);
-			for (int i = 0; i < max; i++)
+			ifstream file(user);
+			if (file.is_open())
 			{
 
-				printf("ERROR ==> %d\n", rand() % (999 - 111) + 111);
-			}
-			col = 11;
-			SetConsoleTextAttribute(hConsole, col);
-			cout << "\n\n";
-		}
-		if (command == "!experimentalMode")
-		{
-			cout << "\n\n";
-			col = 8;
-			SetConsoleTextAttribute(hConsole, col);
-			printf("Do you want to turn on experimental mode? (y/n) ");
-			col = 11;
-			SetConsoleTextAttribute(hConsole, col);
-			cin >> something;
-			col = 8;
-			SetConsoleTextAttribute(hConsole, col);
-			if (something == "y")
-			{
-				printf("starting...\n");
-				printf("[");
-				for (int i = 0; i <= 60; i++)
+				while (getline(file, line))
 				{
-					printf("=");
-					Sleep(2);
+					if (counte == 0 + (8 * i))
+					{
+						col = 8;
+						SetConsoleTextAttribute(hConsole, col);
+						printf("checking...\n");
+						printf("[");
+						for (int i = 0; i <= 60; i++)
+						{
+							printf("=");
+							Sleep(50);
+						}
+						printf("]\n");
+						printf("%d.USER: %s ====> ", i, line.c_str());
+						test = rand() % (10 - 1) + 1;
+						switch (test)
+						{
+						case 1:
+							col = 2;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[online]\n");
+							break;
+						case 2:
+							col = 2;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[online]\n");
+							break;
+						case 3:
+							col = 4;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[offline]\n");
+							break;
+						case 4:
+							col = 2;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[online]\n");
+							break;
+						case 5:
+							col = 4;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[offline]\n");
+							break;
+						case 6:
+							col = 4;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[offline]\n");
+							break;
+						case 7:
+							col = 2;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[online]\n");
+							break;
+						case 8:
+							col = 2;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[online]\n");
+							break;
+						case 9:
+							col = 4;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[ofline]\n");
+							break;
+						case 10:
+							col = 2;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("[online]\n");
+							break;
+						default:
+							col = 4;
+							SetConsoleTextAttribute(hConsole, col);
+							printf("Fatal ERROR!!!\n");
+							break;
+						}
+						cout << "\n";
+						i++;
+
+					}
+					counte++;
+
 				}
-				printf("]\n");
-				col = 2;
-				SetConsoleTextAttribute(hConsole, col);
-				printf("done!!!\n");
-				col = 9;
-				SetConsoleTextAttribute(hConsole, col);
-				system("pause");
-				system("cls");
-				logoShow = true;
-				break;
 			}
-			if (something == "n")
+			else
 			{
-				printf("finishing...\n");
-				printf("[");
-				for (int i = 0; i <= 60; i++)
-				{
-					printf("=");
-					Sleep(2);
-				}
-				printf("]\n");
-				col = 2;
-				SetConsoleTextAttribute(hConsole, col);
-				printf("done!!!\n");
-				col = 11;
-				SetConsoleTextAttribute(hConsole, col);
-				system("pause");
-				system("cls");
-				logoShow = true;
-				break;
-			}
-			else if (something != "n" && something != "y")
-			{
+
 				col = 4;
 				SetConsoleTextAttribute(hConsole, col);
-				printf("ERROR ==> 832\n");
-				col = 11;
-				SetConsoleTextAttribute(hConsole, col);
+				printf("Fatal ERROR!!!\n");
 			}
+			file.close();
+			line = "";
+			cout << "\n\n";
 		}
-		else if ( command != "!experimentalMode" && command != "!errors" && command != "!reset" && command != "!update" && command != "!network" && command != "!help" && command != "!menu")
+		if (command == "!choose")
+		{
+			cout << "\n\n";
+			i = 0;
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			printf("Session number (remember everything starts from 0): ");
+			col = 11;
+			SetConsoleTextAttribute(hConsole, col);
+			cin >> i;
+			col = 8;
+			SetConsoleTextAttribute(hConsole, col);
+			Choose(logo, i);
+			break;
+		}
+		else if (command != "!test" && command != "!choose" && command != "!test" && command != "!closeAll" && command != "!close" && command != "!open" && command != "!help" && command != "!menu")
 		{
 			cout << "\n\n";
 			col = 4;
 			SetConsoleTextAttribute(hConsole, col);
 			printf("unknown command!!!\n");
-			col = 11;
+			col = 9;
 			SetConsoleTextAttribute(hConsole, col);
 			cout << "\n\n";
 		}
 	}
-
-
 }
